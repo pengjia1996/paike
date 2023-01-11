03f8866778a7e5f9b769aec2e00639784d6ae3d6
pengjia <2776248656>
2023-01-11 14:28:07 +0800

    添加maven支持

---
+++ src/main/java/com/pengjia/paike/Main.java
@@ -0,0 +1,8 @@
+package com.pengjia.paike;
+
+/**
+ * @author pengjia
+ * @date 2023/1/11 14:27
+ */
+public class Main {
+}
---
+++ src/test/java/com/pengjia/paike/test/Test.java
@@ -0,0 +1,8 @@
+package com.pengjia.paike.test;
+
+/**
+ * @author pengjia
+ * @date 2023/1/11 14:27
+ */
+public class Test {
+}
